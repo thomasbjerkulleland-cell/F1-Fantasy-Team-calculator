1a747ee74600d1dc361ccc4cd04495d2921a3f5c
Thomas Bjerk Ulleland <thomaull@ulrik.uio.no>
2025-11-08 00:13:41 +0100

    refactoring and code cleanup

---
+++ src/main/java/Constructor.java
@@ -0,0 +1,10 @@
+class Constructor {
+    String name;
+    int price, avgPoints;
+
+    public Constructor(String name, int price, int avgPoints){
+        this.name = name;
+        this.price = price;
+        this.avgPoints = avgPoints;
+    }
+}
---
+++ src/main/java/Driver.java
@@ -0,0 +1,10 @@
+class Driver {
+    String name;
+    int price, avgPoints;
+
+    public Driver(String name, int price, int avgPoints){
+        this.name = name;
+        this.price = price;
+        this.avgPoints = avgPoints;
+    }
+}
